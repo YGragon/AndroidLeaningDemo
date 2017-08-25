492d21ebb8879457bbcf89f2a059bd5aff542a6b
dongxi346 <1105894953@qq.com>
2017-08-25 10:04:34 +0800

    leaning DrawerLayout and NavigationView

--- app/src/main/java/com/dongxi/rxdemo/MainActivity.java
+++ app/src/main/java/com/dongxi/rxdemo/MainActivity.java
@@ -2,7 +2,12 @@ package com.dongxi.rxdemo;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -13,13 +18,39 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity implements Toolbar.OnMenuItemClickListener {
 
     private Toolbar mToolbar;
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mNavigationView;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setStatus() ;
+
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.activity_main);
+        mNavigationView = (NavigationView) findViewById(R.id.nav_view);
+        setupDrawerContent(mNavigationView);
+
+        mNavigationView.setNavigationItemSelectedListener(
+
+                new NavigationView.OnNavigationItemSelectedListener()
+                {
+
+                    private MenuItem mPreMenuItem;
+
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem)
+                    {
+                        if (mPreMenuItem != null) mPreMenuItem.setChecked(false);
+                        menuItem.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        mPreMenuItem = menuItem;
+                        return true;
+                    }
+                });
 
         mToolbar.setTitle("DemoToolbar");
         setSupportActionBar(mToolbar);
@@ -33,18 +64,52 @@ public class MainActivity extends AppCompatActivity implements Toolbar.OnMenuIte
         //设置返回的图标
         mToolbar.setNavigationIcon(getResources().getDrawable(R.mipmap.slide));
         //设置监听
-        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+//        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                finish();
+//            }
+//        });
     }
 
+    //设置状态栏透明
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void setStatus() {
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+    }
+
+    private void setupDrawerContent(NavigationView navigationView)
+    {
+        navigationView.setNavigationItemSelectedListener(
+
+                new NavigationView.OnNavigationItemSelectedListener()
+                {
+
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem)
+                    {
+                        menuItem.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu,menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(item.getItemId() == android.R.id.home)
+        {
+            mDrawerLayout.openDrawer(GravityCompat.START);
+            return true ;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 
@@ -52,7 +117,7 @@ public class MainActivity extends AppCompatActivity implements Toolbar.OnMenuIte
     public boolean onMenuItemClick(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_edit:
-                startActivity(new Intent(MainActivity.this,RxJavaTestActivity.class));
+                startActivity(new Intent(MainActivity.this,MDOneActivity.class));
                 Toast.makeText(this, "menu1", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.action_share:
