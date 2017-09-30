dd01ca7cfd449d291482f8224c0d44c96508153f
Aller_Dong <Aller_Dong@163.com>
2017-09-30 11:09:27 +0800

    add update welcome pager

--- app/src/main/java/com/dongxi/rxdemo/guide/SplashActivity.java
+++ app/src/main/java/com/dongxi/rxdemo/guide/SplashActivity.java
@@ -2,14 +2,14 @@ package com.dongxi.rxdemo.guide;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 
 import com.dongxi.rxdemo.MainActivity;
 import com.dongxi.rxdemo.R;
-import com.dongxi.rxdemo.global.AppConstants;
-import com.dongxi.rxdemo.utils.SpUtils;
 
 /**
  * Created by Administrator on 2017/9/29.
@@ -17,31 +17,52 @@ import com.dongxi.rxdemo.utils.SpUtils;
 
 public class SplashActivity extends Activity {
     private static final String TAG = "SplashActivity";
+    //判断用户是否是第一次使用该应用
+     private boolean isFirstUse = false;
+    //延时时间，用于由欢迎界面进入另外的页面的延时效果
+    private static final int TIME = 2*1000;
+     private static final int TO_MAIN = 100001;
+     private static final int TO_GUIDE = 100002;
 
+    Handler myHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case TO_MAIN:
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            enterHomeActivity();
+                        }
+                    }, 2000);
+                    break;
+                case TO_GUIDE:
+                    Intent intent = new Intent(SplashActivity.this, WelcomeGuideActivity.class);
+                    startActivity(intent);
+                    finish();
+                    break;
+                default:
+
+                    break;
+            }
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // 判断是否是第一次开启应用
-        boolean isFirstOpen = SpUtils.getBoolean(this, AppConstants.FIRST_OPEN);
-        // 如果是第一次启动，则先进入功能引导页
-        if (!isFirstOpen) {
-            Log.e(TAG, "onCreate: welcome");
-            Intent intent = new Intent(this, WelcomeGuideActivity.class);
-            startActivity(intent);
-            finish();
-            return;
-        }
-
-        // 如果不是第一次启动app，则正常显示启动屏
         setContentView(R.layout.activity_splash);
+        SharedPreferences perPreferences = getSharedPreferences("JohnTsai", MODE_PRIVATE);
+        isFirstUse = perPreferences.getBoolean("isFirstUse", true);
 
-        new Handler().postDelayed(new Runnable() {
-
-            @Override
-            public void run() {
-                enterHomeActivity();
-            }
-        }, 2000);
+        if (!isFirstUse) {
+            myHandler.sendEmptyMessageDelayed(TO_MAIN, TIME);
+        }else{
+            myHandler.sendEmptyMessageDelayed(TO_GUIDE, TIME);
+            SharedPreferences.Editor editor = perPreferences.edit();
+            editor.putBoolean("isFirstUse", false);
+            editor.commit();
+        }
     }
 
     private void enterHomeActivity() {
