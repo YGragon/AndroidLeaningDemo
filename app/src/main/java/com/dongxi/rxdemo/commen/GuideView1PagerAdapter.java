2ec4b1fc076bb85767cee58bc32a94c0c8b989d5
dongxi346 <1105894953@qq.com>
2017-10-02 21:42:53 +0800

    add baseviewpageradapter

---
+++ app/src/main/java/com/dongxi/rxdemo/commen/GuideView1PagerAdapter.java
@@ -0,0 +1,21 @@
+package com.dongxi.rxdemo.commen;
+
+import android.view.View;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/9/29.
+ */
+
+public class GuideView1PagerAdapter extends BasePagerAdapter<View> {
+
+    public GuideView1PagerAdapter(List<View> data) {
+        super(data);
+    }
+
+    @Override
+    public View newView(int position) {
+        return null;
+    }
+}
