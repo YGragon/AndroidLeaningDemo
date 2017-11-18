aca8238fc8e10564ea3716433005cc253e6ef4a0
dongxi346 <1105894953@qq.com>
2017-11-18 23:30:13 +0800

    添加kotlin

--- app/src/main/java/com/dongxi/demo/kotlin/KotlinTestActivity.kt
+++ app/src/main/java/com/dongxi/demo/kotlin/KotlinTestActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
 import com.dongxi.rxdemo.R
 import kotlinx.android.synthetic.main.activity_kotlin_learning.*
+import kotlinx.android.synthetic.main.item_thumb_up.*
 
 class KotlinTestActivity : AppCompatActivity() {
 
@@ -24,5 +25,21 @@ class KotlinTestActivity : AppCompatActivity() {
                 Toast.makeText(this,list[i], Toast.LENGTH_SHORT).show()
             }
         }
+        printSum(4,5)
+        Toast.makeText(this,sum(2,3)+"",Toast.LENGTH_SHORT).show()
+    }
+
+    /**
+     * 方法
+     */
+    fun sum(a: Int, b: Int): Int {   // Int 参数，返回值 Int
+        return a + b
+    }
+
+    /**
+     * 无返回值
+     */
+    fun printSum(a: Int, b: Int): Unit {
+        print(a + b)
     }
 }
