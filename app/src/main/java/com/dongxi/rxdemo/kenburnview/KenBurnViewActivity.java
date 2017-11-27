3d507220f5fb8c95ded96a80c1fc7b78205ee857
Aller_Dong <Aller_Dong@163.com>
2017-11-27 11:27:48 +0800

    添加滚动数字效果

--- app/src/main/java/com/dongxi/rxdemo/kenburnview/KenBurnViewActivity.java
+++ app/src/main/java/com/dongxi/rxdemo/kenburnview/KenBurnViewActivity.java
@@ -3,8 +3,10 @@ package com.dongxi.rxdemo.kenburnview;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.dongxi.rxdemo.R;
+import com.dongxi.rxdemo.anim.NumAnim;
 import com.flaviofaria.kenburnsview.KenBurnsView;
 import com.flaviofaria.kenburnsview.Transition;
 
@@ -17,6 +19,10 @@ public class KenBurnViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ken_burn_view);
         KenBurnsView kbv = (KenBurnsView) findViewById(R.id.image);
+        // 滚动的数字
+        TextView runText = (TextView) findViewById(R.id.runText);
+        NumAnim.startAnim(runText, 0.03f);   //第二个参数是textView要显示的价格
+
         kbv.setTransitionListener(new KenBurnsView.TransitionListener() {
             @Override
             public void onTransitionStart(Transition transition) {
